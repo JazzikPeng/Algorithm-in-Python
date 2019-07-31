01dac3c7c6143b1440a94dbc27e8533da6a63d4a
JazzikPeng <zhejianpeng@yahoo.com>
2019-07-31 16:21:42 -0500

    378

---
+++ 378. Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,46 @@
+# Binary_Search Method
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        row,col = len(matrix),len(matrix[0]) # 3, 3
+        l,r = matrix[0][0],matrix[-1][-1] # 1, 15
+        while l + 1 < r:
+            mid = l + (r - l) / 2
+            mid = int(mid)
+            if not self.check(matrix,mid,k):
+                l = mid
+            elif self.check(matrix,mid,k):
+                r = mid
+        
+        if self.check(matrix,l,k):
+            return l
+        if self.check(matrix,r,k):
+            return r
+    
+    def check(self,matrix,target,k):
+        res = 0
+        row,col = len(matrix),len(matrix[0])
+        for i in range(row):
+            idx = self.find_last_noLarger(matrix[i],target)
+            res += idx + 1
+        
+        return res >= k
+    
+    def find_last_noLarger(self,nums,target):
+        l,r = 0,len(nums) - 1
+        while l + 1 < r:
+            mid = l + (r - l) / 2
+            mid = int(mid)
+            if nums[mid] > target:
+                r = mid
+            else:
+                l = mid
+        if nums[r] <= target:
+            return r
+        if nums[l] <= target:
+            return l
+        return -1
