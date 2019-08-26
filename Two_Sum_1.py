d227fbc61526383c2ab3b9a589fcf49efaa371f9
JazzikPeng <zhejianpeng@yahoo.com>
2019-08-26 15:45:42 -0500

    two sum

--- Two_Sum_1.py
+++ Two_Sum_1.py
@@ -15,3 +15,20 @@ class Solution:
                 s.add(target-nums[i])
                 d[nums[i]] = i
         return None
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in nums:
+            nums[nums.index(i)] = 'current'
+            if (target - i) in nums and nums[nums.index(target - i)]!='current':
+                print(i, nums.index('current'), nums)
+
+                nums[nums.index('current')] = 'marked'
+                nums[nums.index(target - i)] = 'other'
+                return [nums.index('marked'), nums.index('other')]
+            nums[nums.index('current')] = 'visited'
