0722934277e18a113131b0422dabd16df276836c
JazzikPeng <zhejianpeng@yahoo.com>
2020-01-02 17:33:17 -0600

    0201

--- 464. Can I Win.py
+++ 464. Can I Win.py
@@ -5,34 +5,31 @@ class Solution(object):
         :type desiredTotal: int
         :rtype: bool
         """
-        
-        if maxChoosableInteger*(maxChoosableInteger+1)/2 < desiredTotal:
+        if maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal:
             return False
-        
         if desiredTotal <= 0:
             return True
         
-        bool_array = [i for i in range(1,maxChoosableInteger+1)]
+        arr_of_nums = [i for i in range(1, maxChoosableInteger+1)]
         
         self.cache = {}
-        
-        return self.minimax(desiredTotal,bool_array)
-    
-    def minimax(self,desiredTotal,visited):
-        
+        return self.minMax(desiredTotal, arr_of_nums)
+
+    def minMax(self, desiredTotal, visited):
         if tuple(visited) in self.cache:
-            return self.cache[tuple(visited)]
+            return self.cache[tuple(visited)] 
         if desiredTotal <= 0:
             return False
         for i in range(len(visited)):
-            temp = visited[i]
-            newV = visited[:i] + visited[i+1:]
-            if not self.minimax(desiredTotal-temp,newV):
+            newGoal = desiredTotal - visited[i]
+            newVisited = visited[:i] + visited[i+1:]
+            if not self.minMax(newGoal, newVisited):
                 self.cache[tuple(visited)] = True
                 return True
         self.cache[tuple(visited)] = False
         return False
 
+
 s = Solution()
-print(s.canIWin(10, 3))
+print(s.canIWin(6, 16))
 print(s.cache)
